5699e2c9e4797cd8f2066b393907d0dba6b07be3
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-08-20 09:22:22 -0300

    Update a.py

--- a.py
+++ a.py
@@ -1,17 +1,22 @@
+
 def circulo(tipo_retorno, raio):
     pi=3.141592
     if tipo_retorno == 'area':
         return pi * raio **2
     elif tipo_retorno == 'perimetro':
         return 1 * pi  * raio
-def retangulo(tipo_retorno largura, altura):
+
+def retangulo(tipo_retorno, largura, altura):
     if tipo_retorno == 'area':
         return largura * altura
     elif tipo_retorno == 'perimetro':
         return 2 * (largura + altura)
     
 def verifica_resultado(largura, altura, raio):
-    circulo("area", 5)
-    circulo("perimetro")
-    retangulo("area", 8, 12)
-    retangulo("perimetro", 8 )
+    print(circulo("area", raio))
+    print(circulo("perimetro", raio))
+    print(retangulo("area", largura, altura))
+    print(retangulo("perimetro", largura , altura))
+
+verifica_resultado()
+
